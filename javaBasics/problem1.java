60fd6ff666dbdb9248c2989c398057fb169d76cd
ashish bongu <bonguashish532222@gmail.com>
2026-02-08 22:55:37 +0530

    created javaBasics Folder

---
+++ javaBasics/problem1.java
@@ -0,0 +1,21 @@
+public class problem1{
+    public static void main(String args[]){
+        int number=767785;
+        int number1=number;
+        int count=0;
+        //Brute force
+
+        while (number !=0 ){
+            number=number/10;
+            count++;
+        }
+
+       System.out.println(count);
+
+
+       //optimal path 
+
+       int result = (int) (Math.log10(number1) + 1);
+       System.out.println(result);
+    }
+}
