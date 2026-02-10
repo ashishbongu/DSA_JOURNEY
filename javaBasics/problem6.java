1c220bebb27a6b6ea061ad90056d8603386cd753
ashish bongu <bonguashish532222@gmail.com>
2026-02-10 23:15:32 +0530

    modified problem6

--- javaBasics/problem6.java
+++ javaBasics/problem6.java
@@ -9,6 +9,21 @@ public class problem6 {
         int number =37;
         System.out.println(Math.sqrt(number));// using java.math 
 
+
+        //MY LEET CODE Q.69 APPROACH - O(n^1/2)  Use BINARY SEARCH TO OPTIMIZE
+
+        // class Solution {
+        //     public int mySqrt(int x) {
         
+        //        long i; 
+        //        for(i=0;i<=x;i++){
+        //             if (i*i > x){
+        //                break;
+        //             }
+        //         }
+        
+        //         return (int) i-1;
+        //     }
+        // }
     }
 }
